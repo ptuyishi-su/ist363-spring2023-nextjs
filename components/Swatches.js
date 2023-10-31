2ee02a5d0686a6386e224c4720b0231da4658b2d
Patrick Tuyishime <patricktuyishime@Patricks-MacBook-Air.local>
2023-10-31 17:21:52 -0400

    fixed error

---
+++ components/Swatches.js
@@ -0,0 +1,19 @@
+import styles from './swatches.module.scsss'
+const Swatches = ({
+    changeHander,
+    data
+}) =>{
+    return <ul className={styles.swatch__list}>
+        {data.map((item)=>{
+            return <li 
+            className={styles.swatch__item}
+            style={{backgroundColor: item}}
+            onClick={()=>{
+                changeHander(item);
+            }}
+        >
+            {item}
+        </li>
+    })}</ul>
+}
+export default Swatches;
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Button from "../components/Buttons";
+import CarColorPicker from '../components/CarColorPicker';
+const Homepage = () => {
+  return <div>
+    <CarColorPicker/>
+
+
+
+    <h1>Homepage</h1>
+    <Button 
+      label="Register now"
+      type='primary'
+    />
+    <Button 
+      label="Download now" 
+      type="secondary"
+    />
+    <Button label="Learn More" />
+
+  </div>
+}
+export default Homepage;

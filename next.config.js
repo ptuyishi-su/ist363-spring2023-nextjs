429b5314a4ba9e750b7f82b839a2140af6dfe934
Patrick Tuyishime <patricktuyishime@Patricks-MacBook-Air.local>
2023-11-28 18:34:55 -0500

    attendance 11/28/23

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'copy-paste-the-url-of-your-local-wp-site-here-without-the-http-and-without-a-trailing-slash'
+        },
+        {
+          protocol: 'https',
+          hostname: 'subaru.ist363.xyz'
+        },
+      ],
+    }
+  }

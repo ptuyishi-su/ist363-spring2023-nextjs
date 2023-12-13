5f2c451544bc05d535a5c94ae1bbc979b721791a
Patrick Tuyishime <patricktuyishime@Patricks-MacBook-Air.local>
2023-12-12 20:53:48 -0500

    error fix 12/12/23

--- components/Grid.js
+++ components/Grid.js
@@ -1,33 +1,47 @@
-import styles from './grid.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
 import { motion } from "framer-motion"
 
 
-const Grid = ({items}) => {
-    const sectionVariants={
-        closed:{
-            y:50,
-            opacity:0
+import styles from './grid.module.scss';
+
+const Grid = ({ items }) => {
+    const sectionVariants = {
+        closed: { 
+            opacity: 0 
         },
-        open:{
-            y:0,
-            opacity:1
-
-
+        open: {
+          opacity: 1,
+          transition: {
+            staggerChildren: 0.2
+          }
         }
     }
+    const articleVariants = {
+        closed: { 
+            y: 50, 
+            opacity: 0 
+        },
+        open: { 
+            y: 0, 
+            opacity: 1 
+        }
+    }
+
     return <motion.section 
-    initial="closed"
-    animate="open"
-    variants={sectionVariants}
-        className={styles.grid}>
-            
-           {items.map((item, index)=>{
-                    const{ title, slug, vehicleInformation }=vehicle.node;
-                    const {trimLevels}= vehicleInformation
+        initial="closed"
+        animate="open"
+        variants={sectionVariants}
+        className={styles.grid}
+    >
+        {items.map((item, index) => {
+                    const { title, slug, vehicleInformation } = item.node;
+                    const { trimLevels } = vehicleInformation;
                     return <motion.article 
                         key={index}
-                        variants={}>
-                        {trimLevels && trimLevels[0].images.thumbnail &&
+                        variants={articleVariants}
+                    >
+                        {trimLevels && trimLevels[0].images.thumbnail && 
                             <Image 
                                 src={trimLevels[0].images.thumbnail.node.sourceUrl}
                                 alt={trimLevels[0].images.thumbnail.node.altText}
@@ -35,15 +49,12 @@ const Grid = ({items}) => {
                                 height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
                             />
                         }
-                        
                         <h3>{title}</h3>
                         <p>
                             <Link href={`/vehicles/${slug}`}>Learn more</Link>
                         </p>
                     </motion.article>
-                }
-                )}
-        {/* {children} */}
+                })}
     </motion.section>
 }
 export default Grid;
--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,31 +1,60 @@
+import { useState } from 'react';
+
 import Container from '../../components/Container';
+import FilterBar from '../../components/FilterBar';
 import Grid from '../../components/Grid';
-import Image from 'next/image';
-import Layout from "../../components/Layout";
-import Link from "next/link";
 
-import { getAllVehicles } from "../../lib/api";
+import Layout from '../../components/Layout';
 
-export async function getStaticProps (){
+
+import { getAllVehicles, getVehicleTypes } from '../../lib/api';
+
+export async function getStaticProps() {
     const vehicles = await getAllVehicles();
-    return{
+    const vehicleTypes = await getVehicleTypes();
+
+    vehicleTypes.unshift({
+        "node": {
+          "name": "All",
+          "slug": "all"
+        }
+      },);
+
+    return {
         props: {
-            vehicles
+            vehicles,
+            vehicleTypes
         }
     }
-
 }
+const VehiclesPage = ({ vehicles, vehicleTypes }) => {
+    // add "all" to vehicleTypes
+    const [activeVehicleType, setActiveVehicleType] = useState('all');
 
-const VehiclePage = ({vehicles })=>{
+    // filter vehicles by activeVehicleType
+    const filteredVehicles = activeVehicleType === 'all' ? 
+        vehicles 
+        : 
+        vehicles.filter((vehicle) => {
+            const { vehicleTypes } = vehicle.node;
+            const vehicleTypeSlugs = vehicleTypes.edges.map((vehicleType) => {
+                return vehicleType.node.slug;
+            });
+            return vehicleTypeSlugs.includes(activeVehicleType);
+     });
+
     return <Layout>
         <h1>Vehicles</h1>
         <Container>
-            <Grid 
-            items={filtered}
+            <FilterBar 
+                items={vehicleTypes} 
+                activeItem={activeVehicleType} 
+                setActiveItem={setActiveVehicleType}
             />
+            <Grid 
+                items={filteredVehicles} 
+            />   
         </Container>
-       
     </Layout>
-}
-
-export default VehiclePage
+}  
+export default VehiclesPage;
